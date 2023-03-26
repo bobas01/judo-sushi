c9bf5f03edea959956f6f6dc3bfc7eeb4ec38408
Sequeira Alexandre <67326944+bobas01@users.noreply.github.com>
2023-03-26 19:55:36 +0200

    last commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let btBurger = document.getElementById('burger');
+let mainMenu = document.getElementById('main-menu');
+let btClose = document.getElementById('close');
+btBurger.addEventListener('click', function () {
+   mainMenu.classList.toggle('active');
+
+})
+btClose.addEventListener('click', function () {
+   mainMenu.classList.toggle('active');
+
+})
